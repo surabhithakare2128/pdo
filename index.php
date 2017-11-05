b84fc9df81973ff90ec2efa99af6291a6d7dd8b7
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-05 18:58:19 -0500

    establishing PDO connection

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$dsn = 'mysql:dbname=st638; host=sql2.njit.edu';
+$user = 'st638';
+$pass = 'adhere54';
+
+try{
+	$dbh = new PDO($dsn, $user, $pass);
+	$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo 'Connection established';
+}catch(PDOException $e){
+	echo 'connection failed: ' . $e -> getMessage();
+}
