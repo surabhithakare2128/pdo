7b9d6c71d66f41acdd892b2ec938f30a036545b9
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-05 22:43:39 -0500

    updatd select

--- select.php
+++ select.php
@@ -1,21 +1,30 @@
 <?php
 
 include 'establishCon.php';
+include 'display.php';
 
 class collection{
+
+
 	public function __construct(){
 		$calledClass=get_called_class();
-		$this->searchRow(6,$cllaedClass);
+		$this->searchRow(6,$calledClass);
 	}
 	
 	public function searchRow($count,$calledClass){
 		$database=connect::getConnect();
-		$sql='SELECT * FROM ' .$calledCLass . 'WHERE ID<' .$count;
+		$sql='SELECT * FROM ' .$calledClass . '  WHERE id<' . $count;
 		$statement=$database->prepare($sql);
 		$statement->execute();
 		$row=$statement->rowCount();
-		echo 'Number of rows are: ' .$row . '<br>';
+		echo 'Number of rows whose user ID is less than 6 are: ' .$row . '<br><br>';
+		$totalRows=$statement->fetchAll();
+		disp::dispTable($totalRows);
 	}
+}
 
-} 
+class accounts extends collection{
+
+
+}
 ?>
