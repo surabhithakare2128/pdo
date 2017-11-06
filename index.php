d5196df93bf98587eafe5e4077834800ad1e005d
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-05 20:56:06 -0500

    index

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
-$dsn = 'mysql:dbname=st638; host=sql2.njit.edu';
-$user = 'st638';
-$pass = 'adhere54';
+include 'establishCon.php';
+include 'select.php';
+include 'display.php';
 
-try{
-	$dbh = new PDO($dsn, $user, $pass);
-	$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo 'Connected successfully. <br>';
-}catch(PDOException $e){
-	echo 'connection failed: ' . $e -> getMessage() . '<br>';
+$obj = new main();
+
+class main{
+	public function __construct(){
+		new accounts;
+	}
 }
+?>
