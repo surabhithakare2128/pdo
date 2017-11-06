01a9d23f96ae1f763e030adb5b4f5b29c5566613
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-05 22:44:55 -0500

    updated display.php

--- display.php
+++ display.php
@@ -1,21 +1,24 @@
 <?php
 
-include 'select.php';
-
 class disp{
-	public function __construct(){
-		$totalRows=$statement->fetchAll();
-		$this->dispTable($totalRows);
+
+ 	public static function dispTable($e){
+        	echo '<table border="4">';
+		echo '<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>';
+		foreach($e as $r){
+			echo "<tr>";
+			echo "<td>".$r['id']."</td>";
+			echo "<td>".$r['email']."</td>";
+			echo "<td>".$r['fname']."</td>";
+			echo "<td>".$r['lname']."</td>";
+			echo "<td>".$r['phone']."</td>";
+			echo "<td>".$r['birthday']."</td>";
+			echo "<td>".$r['gender']."</td>";
+			echo "<td>".$r['password']."</td>";
+		        echo "</tr>";
+		}
+		echo '</table>';
 	}
-
-	public function dispTable($e){
-		echo '<table border=4>';
-
-		print_r($e);
-		echo '</tablle>';
-	}
-	
-
 } 
 
 ?>
